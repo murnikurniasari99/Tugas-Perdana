3288557c91ecb2e59f8c2c29ba04aa9926a0c656
murnikurniasari99 <48238994+murnikurniasari99@users.noreply.github.com>
2019-09-23 11:20:11 +0700

    Add files via upload

---
+++ TugasPerdana17090013/app/src/main/java/com/example/tugasperdana_17090013/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tugasperdana_17090013
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
